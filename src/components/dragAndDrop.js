b023290cc5b87b1f5005d8a308c681e42d455990
ivo2666 <ivailocvetkov365@gmail.com>
2022-01-11 17:57:02 +0200

    fixed: dragAndDrop

--- src/components/dragAndDrop.js
+++ src/components/dragAndDrop.js
@@ -1,61 +1,62 @@
-import React, { Component } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 
-export class DragAndDrop extends Component {
-  state = {
-    drag: false
-  }
-  dropRef = React.createRef()
-  handleDrag = (e) => {
-    e.preventDefault()
-    e.stopPropagation()
-  }
-  handleDragIn = (e) => {
-    e.preventDefault()
-    e.stopPropagation()
-    this.dragCounter++
-    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
-      this.setState({drag: true})
+
+export const DragAndDrop = (props) => {
+    const [dragging,setDragging] = useState(false);
+    const dropRef = useRef();
+
+    const handleDrag = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
     }
-  }
-  handleDragOut = (e) => {
-    e.preventDefault()
-    e.stopPropagation()
-    this.dragCounter--
-    if (this.dragCounter === 0) {
-      this.setState({drag: false})
+    const handleDragIn = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        this.dragCounter++  
+        if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+            setDragging(true);
+          }
     }
-  }
-  handleDrop = (e) => {
-    e.preventDefault()
-    e.stopPropagation()
-    this.setState({drag: false})
-    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
-      this.props.handleDrop(e.dataTransfer.files)
-      e.dataTransfer.clearData()
-      this.dragCounter = 0    
+    const handleDragOut = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        this.dragCounter--
+        if (this.dragCounter > 0) return
+        setDragging(false);
     }
+    const handleDrop = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        setDragging(false) 
+  if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+    props.handleDrop(e.dataTransfer.files) 
+    e.dataTransfer.clearData() 
+    this.dragCounter = 0 
   }
-  componentDidMount() {
-    let div = this.dropRef.current
-    div.addEventListener('dragenter', this.handleDragIn)
-    div.addEventListener('dragleave', this.handleDragOut)
-    div.addEventListener('dragover', this.handleDrag)
-    div.addEventListener('drop', this.handleDrop)
-  }
-  componentWillUnmount() {
-    let div = this.dropRef.current
-    div.removeEventListener('dragenter', this.handleDragIn)
-    div.removeEventListener('dragleave', this.handleDragOut)
-    div.removeEventListener('dragover', this.handleDrag)
-    div.removeEventListener('drop', this.handleDrop)
-  }
-  render() {
+    }
+
+    useEffect(() => {
+        let div = dropRef.current
+        div.addEventListener('dragenter', handleDragIn)
+        div.addEventListener('dragleave', handleDragOut)
+        div.addEventListener('dragover', handleDrag)
+        div.addEventListener('drop', handleDrop)
+        return () => {
+            div.removeEventListener('dragenter', handleDragIn)
+            div.removeEventListener('dragleave', handleDragOut)
+            div.removeEventListener('dragover', handleDrag)
+            div.removeEventListener('drop', handleDrop)
+        }
+    })
+
+
+
     return (
-      <div
-        style={{height:100, padding: 20, position: 'relative'}}
-        ref={this.dropRef}
+        <div
+        style={{height: 100, padding: 20, position: 'relative'}}
+        ref={dropRef}
       >
-        {this.state.dragging &&
+        {dragging &&
           <div 
             style={{
               border: 'dashed grey 4px',
@@ -83,8 +84,7 @@ export class DragAndDrop extends Component {
             </div>
           </div>
         }
-        {this.props.children}
+        {props.children}
       </div>
     )
-  }
 }
